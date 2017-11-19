029352e045c49531576647f4bc93cf7eb0780a37
Anton Kozlov <drakon.mega@gmail.com>
2017-11-19 19:55:56 +0300

    Update toc

--- tocgen.sh
+++ tocgen.sh
@@ -3,6 +3,6 @@ TOC=index.html
 
 echo -n > $TOC
 
-for i in $(find -name '*.html' -and -not -name proto.html -and -not -name index.html | sort -n); do 
-	echo -e "<p><a href=\"$i\">$(basename ${i%.html})</a></p>\n" >> $TOC
+for i in $(find \( -name '*.html' -o -name '*.pdf' \) -and -not -name proto.html -and -not -name index.html | sort -n -k 1.3 ); do
+	echo -e "<p><a href=\"$i\">$(basename ${i%.*})</a></p>\n" >> $TOC
 done
