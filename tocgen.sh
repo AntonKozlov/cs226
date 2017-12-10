09b5a728727ae731ab04777f53815f395872f150
Anton Kozlov <drakon.mega@gmail.com>
2017-12-10 23:12:05 +0300

    Add questions

--- tocgen.sh
+++ tocgen.sh
@@ -1,8 +1,15 @@
+#!/bin/bash
 
 TOC=index.html
 
 echo -n > $TOC
 
-for i in $(find \( -name '*.html' -o -name '*.pdf' \) -and -not -name proto.html -and -not -name index.html | sort -n -k 1.3 ); do
+MASKED_FILES=(
+	index.html
+	proto.html
+	cs226.html
+)
+
+for i in $(find \( -name '*.html' -o -name '*.pdf' \) ${MASKED_FILES[@]/#/-and -not -name } | sort -n -k 1.3 ); do
 	echo -e "<p><a href=\"$i\">$(basename ${i%.*})</a></p>\n" >> $TOC
 done
