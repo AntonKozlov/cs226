f607bfda745d67fda2df9cb28676cfed15ffd9af
Anton Kozlov <drakon.mega@gmail.com>
2018-09-10 00:01:29 +0300

    2018: 2

--- 2/hello.c
+++ 2/hello.c
@@ -1,16 +1,10 @@
 
 #include <string.h>
-#include <stdio.h>
 #include <unistd.h>
 
 int main(int argc, char *argv[]) {
-	const char *defname = "World";
-	const char *pref = "Hello, ";
-	const char *suf = "\n";
-	write(STDOUT_FILENO, pref, strlen(pref));
-	write(STDOUT_FILENO, argv[0], strlen(argv[0]));
-	const char *name = argc > 1 ? argv[1] : defname;
-	write(STDOUT_FILENO, name, strlen(name));
-	write(STDOUT_FILENO, suf, strlen(suf));
+	const char *msg = "Hello, World!\n";
+	const int len = strlen(msg);
+	write(1, msg, len);
 	return 0;
 }
